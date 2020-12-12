6a332593504c519cf7001ce7fb78c51fdc8a2bfb
Elizaveta <kovel.liza@mail.ru>
2020-12-12 17:38:01 +0300

    Add Provider and ShoppingRoom

---
+++ src/src/Provider.java
@@ -0,0 +1,19 @@
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+
+public class Provider {
+    private ArrayList<Pair<Product,Integer>> listOfProductsOfProvider;
+    public Provider()
+    {
+        this.listOfProductsOfProvider = new ArrayList<>();
+    }
+
+    public ArrayList<Pair<Product, Integer>> getListOfProductsOfProvider() {
+        return listOfProductsOfProvider;
+    }
+
+    public void setListOfOfProvider(Product product, int number) {
+        this.listOfProductsOfProvider.add(new Pair<>(product,number));
+    }
+}
---
+++ src/src/ShoppingRoom.java
@@ -0,0 +1,19 @@
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+
+public class ShoppingRoom {
+    private ArrayList<Pair<Product,Integer>> listOfProductsInShop;
+    public void ShoppinRoom()
+    {
+        this.listOfProductsInShop = new ArrayList<>();
+    }
+
+    public ArrayList<Pair<Product, Integer>> getListOfProductsInShop() {
+        return listOfProductsInShop;
+    }
+
+    public void setListOfProductsInShop(Product product,int number) {
+        this.listOfProductsInShop.add(new Pair<>(product,number));
+    }
+}
